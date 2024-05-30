338d613203cd03fe5319dcbd28b570ddca673421
0x_Anakin <{ID}+{username}@users.noreply.github.com>
2024-05-30 13:34:11 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <regex>
+#include <windows.h>
+
+int SEC = 1000;
+char ARR[11] = "0123456789";
+
+// Check if a char array contains a character
+bool contains(const char* arr, size_t len, const char& ch)
+{
+    bool result = false;
+    // Iterate over the char array
+    for (int i = 0; i < len; i++)
+    {
+        // Check if element at ith index in array
+        // is equal to the given character
+        if (arr[i] == ch)
+        {
+            // Match found
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+bool is_valid_integer_integer(const std::string& str) 
+{    
+    if (str.empty() || str[0] == '0') {
+        return false;
+    }
+
+    for (int i = 0; i < str.size(); i++) {
+        if (!contains(ARR, strlen(ARR), str[i])) {
+            return false;
+        } 
+    }
+
+    return true;
+}
+
+int main(int argc, char* argv[])
+{   
+
+    int min = SEC * 60; // 1 minutes
+    int max = SEC * 60 * 5;  // 5 minutes
+
+    if (argc > 1) 
+    {
+        if (is_valid_integer_integer(argv[1]))
+        {   
+            min = (SEC * 60) * atoi(argv[1]);
+        }
+
+        if (argc > 2)
+        {
+            if (is_valid_integer_integer(argv[2]))
+            {
+                int new_max = (SEC * 60) * atoi(argv[2]);
+                // in case new_max is not bigger than min, adjust it
+                max = new_max > min ? new_max : min + (SEC * 60);
+                
+            }
+        }
+        else 
+        {
+            // if no second argument make max 1 minute bigger
+            max = min + (SEC * 60); 
+        }
+
+    }
+    
+    std::cout << "interval range: " << ((min / 1000) / 60) << "mins - " << ((max / 1000) / 60) << "mins" << std::endl;
+
+    while (true) {
+
+        int interval = rand() % (max - min + 1) + min;
+        // Create an array of INPUT structures to hold the inputs
+        INPUT inputs[2] = {};
+
+        // Set up the INPUT structure for a key press event
+        inputs[0].type = INPUT_KEYBOARD;  // Specify input type as keyboard
+        inputs[0].ki.wVk = VK_SPACE;      // Virtual-key code for the SPACE key
+
+        // Set up the INPUT structure for a key release event
+        inputs[1].type = INPUT_KEYBOARD;  // Specify input type as keyboard
+        inputs[1].ki.wVk = VK_SPACE;      // Virtual-key code for the SPACE key
+        inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;  // Specify key release event
+
+        // Send the inputs to the system
+        UINT uSent = SendInput(2, inputs, sizeof(INPUT));
+
+        if (uSent != 2)  // Check if all inputs were successfully sent
+        {
+            // If the function fails, it returns 0
+            // MessageBox(NULL, L"Failed to send SPACE key input.", L"Error", MB_OK | MB_ICONERROR);
+            std::cerr << "Failed to send SPACE key input" << std::endl;
+        }
+
+        std::cout << "Sleeping for " << ((interval/1000)/60)  << "mins" << std::endl;
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+
+    }
+
+    exit(0);
+
+}
