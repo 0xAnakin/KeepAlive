922f01fd6d5095587278cd88bde818ef3fbf4b12
0x_Anakin <{ID}+{username}@users.noreply.github.com>
2024-05-31 07:48:40 +0300

    fixes -> ms converted to minutes

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-#include <regex>
+#include <ctime>
 #include <windows.h>
 
 int SEC = 1000;
@@ -44,39 +44,43 @@ bool is_valid_integer_integer(const std::string& str)
 int main(int argc, char* argv[])
 {   
 
-    int min = SEC * 60; // 1 minutes
-    int max = SEC * 60 * 5;  // 5 minutes
+    int min = 1; // 1 minutes
+    int max = 5;  // 5 minutes
 
     if (argc > 1) 
     {
         if (is_valid_integer_integer(argv[1]))
         {   
-            min = (SEC * 60) * atoi(argv[1]);
+            min = atoi(argv[1]);
         }
 
         if (argc > 2)
         {
             if (is_valid_integer_integer(argv[2]))
             {
-                int new_max = (SEC * 60) * atoi(argv[2]);
+                int new_max = atoi(argv[2]);
                 // in case new_max is not bigger than min, adjust it
-                max = new_max > min ? new_max : min + (SEC * 60);
+                max = new_max > min ? new_max : min + 1;
                 
             }
         }
         else 
         {
             // if no second argument make max 1 minute bigger
-            max = min + (SEC * 60); 
+            max = min + 1; 
         }
 
     }
     
-    std::cout << "interval range: " << ((min / 1000) / 60) << "mins - " << ((max / 1000) / 60) << "mins" << std::endl;
+    std::cout << "interval range: " << min << "mins - " << max << "mins" << std::endl;
+
+    std::srand(std::time(0));
 
     while (true) {
-
-        int interval = rand() % (max - min + 1) + min;
+        
+        // sleep interval
+        int interval_in_minutes = std::rand() % (max - min + 1) + min;
+        /*
         // Create an array of INPUT structures to hold the inputs
         INPUT inputs[2] = {};
 
@@ -98,11 +102,11 @@ int main(int argc, char* argv[])
             // MessageBox(NULL, L"Failed to send SPACE key input.", L"Error", MB_OK | MB_ICONERROR);
             std::cerr << "Failed to send SPACE key input" << std::endl;
         }
-
-        std::cout << "Sleeping for " << ((interval/1000)/60)  << "mins" << std::endl;
+        */
+        std::cout << "Sleeping for " << interval_in_minutes << "mins" << std::endl;
         
-        std::this_thread::sleep_for(std::chrono::milliseconds(interval));
-
+        std::this_thread::sleep_for(std::chrono::minutes(interval_in_minutes));
+        
     }
 
     exit(0);
